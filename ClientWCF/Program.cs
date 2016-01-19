2c37a3a48874dd704851290677d6bbd7dfd055e8
Didier Duchamp <didier.duchamp@telecomnancy>
2016-01-19 17:39:55 +0100

    Correction bug version fichier

---
+++ ClientWCF/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+
+
+namespace ClientWCF
+{
+    class Program
+    {
+        //Pour tester le service
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ServiceDemandeArticle(7000243));
+            Console.ReadLine();
+
+        }
+        static string ServiceDemandeArticle(int id)
+        {
+            try
+            {
+                ClientWCF.ServiceReference.Service1Client client = new ClientWCF.ServiceReference.Service1Client();
+                return client.DemandeArticle(id);
+            }
+            catch (Exception e)
+            {
+                return e.ToString();
+            }
+        }
+    }
+}
